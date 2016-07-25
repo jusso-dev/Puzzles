a91645f930aa0520285253ebb5a61c640eb9d666
Justin_PC <Justin_PC@JUSTIN>
2016-07-25 13:01:48 +1000

    Commit MVC project

---
+++ Puzzles/Puzzles/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Products.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Puzzles
+        public ActionResult Puzzles()
+        {
+            return View();
+        }
+    }
+}
