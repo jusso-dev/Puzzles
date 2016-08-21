fbe2c9042ac7482d89f72b6b5f3ecce63f2bc88d
jusso-dev <jusso15@gmail.com>
2016-08-21 14:23:57 +1000

    Separated JS into standalone file, changed link on Navbar to redirect page, wired up functionality to clear contents on page

---
+++ Puzzles/Scripts/ajaxTesting.js
@@ -0,0 +1,22 @@
+﻿$(document)
+    .ready(function() {
+        $("#getPuzzleBtn")
+            .click(function() {
+                $.ajax({
+                        url: "http://localhost:49770/api/Puzzles",
+                        dataType: "json"
+                    })
+                    .success(function(data) {
+                        try {
+                            $("#ul").append(JSON.stringify(data));
+                        } catch (ex) {
+                            alert("Ajax call failed " + ex.message);
+                        }
+
+                        $("#clearPuzzleBtn").click(function () {
+                             $("#ul").empty();
+                        });
+                    });
+            });
+   });
+  
